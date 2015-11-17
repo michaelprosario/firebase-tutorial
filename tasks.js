3bc7667d5a1bd5a261d13c07daef82651106b45a
Michael Rosario <michael.p.rosario@gmail.com>
2015-11-16 22:39:38 -0500

    adding hints
    adding hints

--- tasks.js
+++ tasks.js
@@ -18,25 +18,34 @@ var Tasks = function()
 	
 	this.Add = function(aTask)
 	{
+		//hints
+		// https://www.firebase.com/docs/web/api/firebase/push.html
+		// https://www.firebase.com/docs/web/api/firebase/key.html
+		
 		postID = "junk";
 		return postID;
 	}
 	
 	this.Update = function(aTask, strRecordID)
 	{
+		// hint - https://www.firebase.com/docs/web/api/firebase/set.html
 	}
 	
 	this.Delete = function(strRecordID)
 	{
+		//hint - https://www.firebase.com/docs/web/api/firebase/remove.html
 	}
 	
 	this.RecordExists = function(strRecordID)
 	{
+		//hint http://code.runnable.com/Umso4y4z27JEAAAx/detecting-if-data-exists-in-firebase-for-javascript
 	}
 	
 	
 	this.GetRecord = function(strRecordID)
 	{
+		// hint: https://www.firebase.com/blog/2013-10-01-queries-part-one.html
+		
 		var objData = null;		
 		return objData;
 	}
